d66acfd7ca0600d2e041f410be0367e5ba4576ae
kelvinpraises <kelvinpraises@gmail.com>
2026-02-08 06:07:54 +0100

    feat(server): add neural tools for multichain queries

---
+++ apps/server/src/interfaces/neural/tools/multichain/index.ts
@@ -0,0 +1,2 @@
+export { getSupportedTokens } from "@/interfaces/neural/tools/multichain/items/get-supported-tokens";
+export { getSupportedChains } from "@/interfaces/neural/tools/multichain/items/get-supported-chains";
