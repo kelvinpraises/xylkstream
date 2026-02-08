c500f95ace04d5e4912a3a528ff07f147dfb3587
kelvinpraises <kelvinpraises@gmail.com>
2026-02-08 06:08:05 +0100

    feat(server): register bridge, swap, and multichain tools in neural agent

--- apps/server/src/interfaces/neural/tools/index.ts
+++ apps/server/src/interfaces/neural/tools/index.ts
@@ -1,9 +1,15 @@
 import * as accountTools from "@/interfaces/neural/tools/account";
 import * as journalTools from "@/interfaces/neural/tools/journal";
 import * as streamTools from "@/interfaces/neural/tools/stream";
+import * as swapTools from "@/interfaces/neural/tools/swap";
+import * as bridgeTools from "@/interfaces/neural/tools/bridge";
+import * as multichainTools from "@/interfaces/neural/tools/multichain";
 
 export const allTools = {
   ...accountTools,
   ...journalTools,
   ...streamTools,
+  ...swapTools,
+  ...bridgeTools,
+  ...multichainTools,
 };
