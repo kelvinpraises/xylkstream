eeec2f0e29ecfd42fe289e4f421d15f058419f63
kelvinpraises <kelvinpraises@gmail.com>
2026-02-08 06:07:42 +0100

    feat(server): add neural tools for swap operations

---
+++ apps/server/src/interfaces/neural/tools/swap/index.ts
@@ -0,0 +1,3 @@
+export { getSwapQuote } from "@/interfaces/neural/tools/swap/items/get-swap-quote";
+export { executeSwap } from "@/interfaces/neural/tools/swap/items/execute-swap";
+export { findBestSwapRoute } from "@/interfaces/neural/tools/swap/items/find-best-swap-route";
